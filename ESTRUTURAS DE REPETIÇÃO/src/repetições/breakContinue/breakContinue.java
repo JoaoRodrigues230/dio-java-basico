42785434e87b1db68525be9ffa8b1d5f11ca0518
João Rodrigues <jvp.pereira20@gmail.com>
2023-10-14 16:16:54 -0300

    condições de repetição

---
+++ ESTRUTURAS DE REPETIÇÃO/src/repetições/breakContinue/breakContinue.java
@@ -0,0 +1,11 @@
+package repetições.breakContinue;
+
+public class breakContinue {
+    public static void main(String[] args) {
+        for(int num = 1; num <= 5; num++){
+            if(num==3)
+                break;
+            System.out.println(num);
+        }
+    }
+}
---
+++ ESTRUTURAS DE REPETIÇÃO/src/repetições/forArrey/forArrey.java
@@ -0,0 +1,11 @@
+package repetições.forArrey;
+
+public class forArrey {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE","JONAS","JULIA","MARCOS"};
+
+        for(int x=0;x<alunos.length;x++){
+            System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+        }
+    }
+}
